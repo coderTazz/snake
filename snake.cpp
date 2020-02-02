ec3d38cb958cd75bde3059bf31e8eed1f8eecbb5
coderTazz <vdvinayak@gmail.com>
2020-02-02 18:05:16 -0500

    fruit class added and working

--- snake.cpp
+++ snake.cpp
@@ -1,10 +1,41 @@
-# include<iostream>
+# include <iostream>
+# include <ctime>
 
 using namespace std;
 
+class cFruit {
+private:
+	int posX;
+	int posY;
+
+public:
+
+	cFruit(int w, int h) {
+		spawn(w, h);
+	}
+
+	void spawn(int w, int h) {
+		posX = rand()%(w - 1) + 1;
+		posY = rand()%(h - 1) + 1;
+	}
+
+	inline int getX() { return posX; }
+	inline int getY() { return posY; }
+
+	friend ostream & operator<<(ostream & o, cFruit c) {
+		o << "Fruit: " << "[" << c.posX << ", " << c.posY << "]" << endl;
+		return o;
+	}
+};
+
+
 int main() {
-
-	cout << "I am going to write a game!\n";
+	srand(time(NULL));
+	cFruit cF(50, 50);
+	cout << cF;
+	cF.spawn(50, 50);
+	cout << cF;
+	cF.spawn(50, 50);
+	cout << cF;
 	return 0;
-
 }
